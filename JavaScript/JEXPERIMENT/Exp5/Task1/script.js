de309d5572f2f6c438fed73888bbefbaff0a8b96
CosmicCoder06 <k.avhi4612@gmail.com>
2025-10-29 16:51:49 +0530

    new commit

---
+++ JavaScript/JEXPERIMENT/Exp5/Task1/script.js
@@ -0,0 +1,3 @@
+// alert("Hello World");
+// const usermane = prompt("Hello welcome to my stupid webpage, What's your name: ");
+// alert("Hello " + usermane + ", nice to meet you");
---
+++ JavaScript/JEXPERIMENT/Exp5/Task2/script.js
@@ -0,0 +1,3 @@
+let age = prompt("Enter your age: ");
+age = Number(age);
+age >= 18 ? alert("<h1>You are eligible to vote.</h1>") : alert("<h2>You are minor.</h2>");
