e340c1aa80ed0bd2b987ed7e6597c0487c4cb114
CosmicCoder06 <k.avhi4612@gmail.com>
2025-11-03 14:04:57 +0530

    WorkShop

--- JavaScript/JEXPERIMENT/Exp5/Task2/script.js
+++ JavaScript/JEXPERIMENT/Exp5/Task2/script.js
@@ -1,3 +1,7 @@
-let age = prompt("Enter your age: ");
-age = Number(age);
-age >= 18 ? alert("<h1>You are eligible to vote.</h1>") : alert("<h2>You are minor.</h2>");
+// let age = prompt("Enter your age: ");
+// age = Number(age);
+// age >= 18 ? alert("<h1>You are eligible to vote.</h1>") : alert("<h2>You are minor.</h2>");
+const arr=[1,2,3,5];
+const st=arr.shift();
+console.log(st);
+console.log(arr);
---
+++ JavaScript/Session/S1.js
@@ -0,0 +1,38 @@
+// let n = 5;
+// if (n>3){
+//     let n = 10;
+//     console.log()
+// }
+
+// const oddEven = (n) => n % 2  === 1;
+// console.log(oddEven(5));
+
+// alert(`Hello, ${prompt('Enter your name: ')}! Welcome!`);
+
+// const user  = "aman";
+// const points = "120";
+// const message = "Hey" + user + ", you have " + "points!";
+// console.log(message);
+
+// const person = {
+//     firstName : "Nitin",
+//     lastName : "Sharma",
+//     age : 121,
+//     introduce: function () {
+//         console.log(`Hi my name is ${this.firstName} ${this.lastName} and i am ${this.age} years old`);
+//     }
+// };
+
+// // console.log(person.firstName);
+// person.introduce();
+// console.log(person["firstName"]);
+// const hisAge = "age";
+// console.log(person[hisAge]);
+
+const person = {
+    firstName : "Nitin",
+    lastName : "Sharma",
+    age : 121,
+};
+
+person.birthyear = 2025 - person.age;
